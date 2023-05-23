a9782370ccc17bebf0c33a48b93858c85b9b20a0
unknown <umarvayevxusan@gmail.com>
2023-05-23 18:03:43 +0500

    Second commit

--- app/Http/Livewire/StudentsComponent.php
+++ app/Http/Livewire/StudentsComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Students;
 
 class StudentsComponent extends Component
 {
@@ -15,12 +16,30 @@ class StudentsComponent extends Component
         $this->validateOnly($fields, [
             'student_id' => 'required|unique:students', //Students = table name
             'name' => 'required',
-            'email' => 'required',
+            'email' => 'required|email',
             'phone' => 'required|numberic',
         ]);
     }
     public function storeStudentData()
     {
+        // on form submit validation
+        $this->validate([
+            'student_id' => 'required|unique:students', //Students = table name
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|numberic',
+        ]);
+
+        //Add Student Data
+        $student = new Students();
+        $student->student_id = $this->student_id;
+        $student->name = $this->name;
+        $student->email = $this->email;
+        $student->phone = $this->phone;
+
+        $student->save();
+
+        session()->flash('massege','New student has been added successfully');
 
     }
     public function render()
